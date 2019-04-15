1676747b742a71d3c51cd5b49bd2764c9a9b62df
S Boyd <boyds@southwestern.edu>
2019-04-15 14:47:27 -0500

    followed tutorial to train network for clothing recognition

---
+++ tensorflow_example.py
@@ -0,0 +1,120 @@
+# Tutorial used: https://www.tensorflow.org/tutorials/keras/basic_classification
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.datasets import fashion_mnist
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# x_train and y_train are Numpy arrays --just like in the Scikit-Learn API.
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Preprocess data
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+# scale values 0 to 1
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# Verify can display the class name below image
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+# I think the following few sections can be used to tweak performance
+
+# other ideas for manipulation from https://keras.io/#getting-started-30-seconds-to-keras
+# model = Sequential()
+# model.add(Dense(units=64, activation='relu', input_dim=100))
+# model.add(Dense(units=10, activation='softmax'))
+
+# model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+# model.compile(loss=keras.losses.categorical_crossentropy,
+#               optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
+
+# Setup the layers of network
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+# Compile the model
+model.compile(optimizer='adam', 
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images, train_labels, epochs=5)
+
+# Evaluate accuracy
+# test_loss, test_acc = model.evaluate(test_images, test_labels)
+# print('Test accuracy:', test_acc)
+
+# Make predictions
+predictions = model.predict(test_images)
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+  
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1]) 
+  predicted_label = np.argmax(predictions_array)
+ 
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+# Plot the first X test images, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions, test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions, test_labels)
+plt.show()
+
